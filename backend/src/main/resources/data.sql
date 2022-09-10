666c4b0aefe9deabfe15fe15a17cf50b6115eb3b
João Victor Costa Santos <rm88372@fiap.com.br>
2022-09-10 20:10:46 -0300

    Entity Offer.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,5 +14,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://live.staticflickr.com/7698/26585736084_24810e6cc6_b.jpg', 'https://png.pngtree.com/png-vector/20190411/ourlarge/pngtree-vector-globe-internet-web-online-monitor-icon-png-image_925332.jpg')
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://live.staticflickr.com/7698/26585736084_24810e6cc6_b.jpg', 'https://png.pngtree.com/png-vector/20190411/ourlarge/pngtree-vector-globe-internet-web-online-monitor-icon-png-image_925332.jpg');
 
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1 );
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1 );
